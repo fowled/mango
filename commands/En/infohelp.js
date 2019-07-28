d4ca2b356fe9266ea0cbe03ffa9df7397e1e11b2
Max <ma15fo43@gmail.com>
2019-07-28 19:56:06 +0200

    Help message translated

--- commands/En/infohelp.js
+++ commands/En/infohelp.js
@@ -1,13 +1,13 @@
 const DISCORD = require("discord.js");
 
 exports.run = async (client, message, args, ops) => {
-    reponse = new DISCORD.RichEmbed()
-        .setTitle(`Aide pour le bot Mazz'Bot`)
+    helpMessage = new DISCORD.RichEmbed()
+        .setTitle(`Help!`)
         .setAuthor(message.author.username, message.author.avatarURL)
         .setColor(Math.floor(Math.random() * 16777214) + 1)
-        .setDescription("**__Commandes de modération:__**\n`ban, kick, warn, clear, pin`\n\n**__Commandes de serveur:__**\n`userinfo, servinfo, invite`\n\n**__Autres commandes:__**\n`dire, infoping, uptime, play, leave, musicinfo`\n\n**__Commandes liées à une API:__**\n`scratchcount, scmessages, scproj, scuser`")
+        .setDescription("**__Moderation commands:__**\n`ban, kick, warn, clear, pin`\n\n**__Guild commands:__**\n`userinfo, guildinfo, invite`\n\n**__Other commands:__**\n`say, infoping, uptime, play, leave, musicinfo`\n\n**__Scratch commands:__**\n`scratchcount, scmessages, scproj, scuser`")
         .setThumbnail(client.user.avatarURL)
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp()
-    client.users.get(message.author.id).send(reponse).catch(console.error).then(message.reply("Message contenant l'aide envoyé."));
+    client.users.get(message.author.id).send(helpMessage).catch(console.error).then(message.reply(":thumbsup: Help message just sent!"));
 }
