e1ce321a8b56d0478e2b76f167c397fe1927182b
Max <ma15fo43@gmail.com>
2019-07-28 19:19:52 +0200

    Translated the ban command

--- commands/En/ban.js
+++ commands/En/ban.js
@@ -1,99 +1,75 @@
 const DISCORD = require("discord.js");
-const BOT = new DISCORD.Client();
 
-// Commande de modération
+// Moderation command
 
-exports.run = (BOT, message, args, tools) => {
-    var userlist = message.mentions.users;
-    var usercount = 0;
+exports.run = (client, message, args, tools) => {
 
-    if (userlist.size != 0) {
-        userlist.forEach(function (user) {
-            usercount++;
-        });
+    var reason = args[2].join(" ");
 
-        var raison = message.content.split(" ");
-        raison = raison.slice(usercount + 1);
-        raison = raison.join(" ");
+    if (reason == "") {
+        reason = "No reason provided";
+    }
 
-        if (raison == "") {
-            raison = "No reason entered.";
+    const userBan = message.mentions.users.first();
+    const memberBan = message.guild.member(userBan);
+
+    if (memberBan) {
+        var banMessageAuthor = message.author.tag;
+        var banGuildName = message.member.guild.name;
+        var guildIcon = message.member.guild.iconURL;
+        var bannedUserId = user.id;
+        var date = new Date;
+
+        if (memberBan.bannable && memberBan.id != "352158391038377984") {
+            reponse = new DISCORD.RichEmbed()
+                .setTitle(`Ban from the server **${banGuildName}**, on the date __${date.toLocaleTimeString()}__`)
+                .setDescription(`You have been banned from the server **${banGuildName}** by **${banMessageAuthor}** on the date __${date}__! Reason: *"${reason}"*`)
+                .setTimestamp()
+                .setThumbnail(guildIcon)
+                .setColor("#4292f4")
+                .setFooter(BOT.user.username, BOT.user.avatarURL)
+            BOT.users.get(bannedUserId).send(reponse);
         }
 
-        userlist.forEach(function (user) {
-            const memberBan = message.guild.member(user);
-            const userBan = message.mentions.users.first();
-
-            if (memberBan) {
-                var authorMessBan = message.author.tag;
-                var serveurBan = message.member.guild.name;
-                var serverIcon = message.member.guild.iconURL;
-                var userBanID = user.id;
-                var date = new Date;
-
-                if (memberBan.bannable && memberBan.id != "352158391038377984") {
-                    reponse = new DISCORD.RichEmbed()
-                        .setTitle(`Ban from the server **${serveurBan}**, on the date __${date}__`)
-                        .setDescription(`Vous avez été banni du serveur **${serveurBan}** par **${authorMessBan}** à la date __${date}__ ! Raison : *"${raison}"*`)
+        setTimeout(function () {
+            if (memberBan.id != "352158391038377984") {
+                memberBan.ban({
+                    reason: reason,
+                }).then(() => {
+                    banMessageGuild = new DISCORD.RichEmbed()
+                        .setTitle(`User **${userBan.username}** is now banned!`)
+                        .setAuthor(message.author.username, message.author.avatarURL)
+                        .setDescription(`:white_check_mark: **${memberBan.user.tag}** is now banned (*${reason}*)!`)
                         .setTimestamp()
-                        .setThumbnail(serverIcon)
                         .setColor("#4292f4")
                         .setFooter(BOT.user.username, BOT.user.avatarURL)
-                    BOT.users.get(userBanID).send(reponse);
-                }
+                    message.channel.send(reponse2);
 
-                setTimeout(function () {
-                    if (memberBan.id != "352158391038377984") {
-                        memberBan.ban({
-                            reason: raison,
-                        }).then(() => {
-                            reponse2 = new DISCORD.RichEmbed()
-                                .setTitle(`Banissement de l'utilisateur **${userBan.username}**`)
-                                .setAuthor(message.author.username, message.author.avatarURL)
-                                .setDescription(`Le banissement de l'utilisateur **${memberBan.user.tag}** avec la raison *"${raison}"* a bien été effectué à : __${date}__ !`)
-                                .setTimestamp()
-                                .setThumbnail("https://vignette.wikia.nocookie.net/mysingingmonsters/images/6/6d/Ban-hammer-newB.jpg/revision/latest?cb=20170520125046")
-                                .setColor("#4292f4")
-                                .setFooter(BOT.user.username, BOT.user.avatarURL)
-                            message.channel.send(reponse2);
-
-                        }).catch(err => {
-                            reponse = new DISCORD.RichEmbed()
-                                .setTitle(`:octagonal_sign: Erreur : banissement de l'utilisateur **${memberBan.user.username}**`)
-                                .setAuthor(message.author.username, message.author.avatarURL)
-                                .setDescription(`Une erreur de permissions a eu lieu en bannissant l'utilisateur **${memberBan.user.tag}** avec la raison *"${raison}"* : err_missing_permissions:couldn't_ban_user.`)
-                                .setTimestamp()
-                                .setThumbnail("")
-                                .setColor("#FF0000")
-                                .setFooter(BOT.user.username, BOT.user.avatarURL)
-                            message.channel.send(reponse);
-                            console.log(err);
-                        });
-                    } else {
-                        reponse = new DISCORD.RichEmbed()
-                            .setTitle(`Hop là ! Au nom de la loi, j'arrête **${message.author.username}**.`)
-                            .setAuthor(message.author.username, message.author.avatarURL)
-                            .setDescription(`On ne bannit pas le **maître suprême**, et le **créateur du bot** !`)
-                            .setTimestamp()
-                            .setThumbnail("")
-                            .setColor("#FF0000")
-                            .setFooter(BOT.user.username, BOT.user.avatarURL)
-                        message.channel.send(reponse);
-                    }
-                }, 500);
+                }).catch(err => {
+                    reponse = new DISCORD.RichEmbed()
+                        .setTitle(`Error`)
+                        .setAuthor(message.author.username, message.author.avatarURL)
+                        .setDescription(`An error has occured while banning **${memberBan.user.tag}**; missing permissions. Please, I am a serious bot, I can have admin rank!`)
+                        .setTimestamp()
+                        .setColor("#FF0000")
+                        .setFooter(BOT.user.username, BOT.user.avatarURL)
+                    message.channel.send(reponse);
+                    console.log(err);
+                });
             } else {
-                message.reply('Une erreur est survenue : l\'utilisateur mentionné n\'est pas dans le serveur !');
+                reponse = new DISCORD.RichEmbed()
+                    .setTitle(`Herm...`)
+                    .setAuthor(message.author.username, message.author.avatarURL)
+                    .setDescription(`You can't ban me, I'm the bot developer!`)
+                    .setTimestamp()
+                    .setThumbnail("")
+                    .setColor("#FF0000")
+                    .setFooter(BOT.user.username, BOT.user.avatarURL)
+                message.channel.send(reponse);
             }
-        });
+        }, 500);
     } else {
-        reponse = new DISCORD.RichEmbed()
-            .setTitle(`:octagonal_sign: Erreur !`)
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .setDescription(`Vous n'avez pas mentionné de membre(s) à bannir !`)
-            .setTimestamp()
-            .setThumbnail("")
-            .setColor("#FF0000")
-            .setFooter(BOT.user.username, BOT.user.avatarURL)
-        message.channel.send(reponse);
+        message.reply('Whoops, please select a member. Ban hammer is waiting!');
     }
+
 }
