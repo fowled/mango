9c49fb54d5dcc3ce0ca5a6acd9a0922eac279532
unknown <ma15fo43@gmail.com>
2019-07-31 23:06:49 +0200

    Translated ping.js

--- commands/En/ping.js
+++ commands/En/ping.js
@@ -1,17 +1,17 @@
 const DISCORD = require("discord.js");
 
 exports.run = async (client, message, args, ops) => {
-    let m = await message.channel.send("Ping?");
+    let ping = await message.channel.send("Ping?");
 
-    let rep = new DISCORD.RichEmbed()
-        .setTitle(`Informations de latence pour l'utilisateur ${message.author.tag}`)
+    let pong = new DISCORD.RichEmbed()
+        .setTitle(`Latency information for ${message.author.tag}`)
         .setAuthor(message.author.username, message.author.avatarURL)
         .setColor(Math.floor(Math.random() * 16777214) + 1)
-        .setDescription("Informations de latence")
-        .addField("Latence hébergeur", `**${m.createdTimestamp - message.createdTimestamp}** ms.`, true)
-        .addField("Latence API", `**${Math.round(client.ping)}** ms.`, true)
+        .setDescription("Latency information")
+        .addField("Host latency", `**${m.createdTimestamp - message.createdTimestamp}** ms.`, true)
+        .addField("API latency", `**${Math.round(client.ping)}** ms.`, true)
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp()
 
-    m.edit(rep);
+    ping.edit(pong);
 }
