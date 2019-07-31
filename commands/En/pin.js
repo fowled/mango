b08c38afb99ea4cfde3836820e0186cabf1b562c
unknown <ma15fo43@gmail.com>
2019-07-31 23:06:24 +0200

    Translated pin.js

--- commands/En/pin.js
+++ commands/En/pin.js
@@ -1,9 +1,9 @@
-// Commande de modération
+// Moderation command
 
 exports.run = async (client, message, args, ops) => {
     message.channel.fetchMessage(args[0])
         .then(message => message.pin())
         .catch(function () {
-            message.reply("Le message n'a pas pu être épinglé.");
+            message.reply("An error occured, make sure I have the pin permission.");
         });
 }
