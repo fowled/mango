690a8509266e3a886d14da70842e76f152b3521a
unknown <ma15fo43@gmail.com>
2019-07-31 23:07:14 +0200

    Translated queue.js

--- commands/En/queue.js
+++ commands/En/queue.js
@@ -1,37 +1,39 @@
 const DISCORD = require("discord.js");
 
+// Music command
+
 exports.run = async (client, message, args, ops) => {
     let fetched = ops.active.get(message.guild.id);
 
-    if (!fetched) return message.channel.send("Il n'y a pour l'instant aucune musique de jouée !");
+    if (!fetched) return message.channel.send("No music is currently played! Come join and add some!");
 
     let queue = fetched.queue;
     let nowPlaying = queue[0];
-    let resp = '';
+    let listMessage = '';
 
     for (var i = 1; i < queue.length; i++) {
-        resp += `${i}- *${queue[i].songTitle}* - Demandé par **${queue[i].requester}**\n`;
+        listMessage += `${i}- *${queue[i].songTitle}* - Demandé par **${queue[i].requester}**\n`;
     }
 
-    if (resp == undefined) {
-        let reponse = new DISCORD.RichEmbed()
-            .setTitle(`Commande queue pour ${message.author.tag}`)
-            .addField(`En train de jouer`, `*${nowPlaying.songTitle}* - Demandé par **${nowPlaying.requester}**`)
+    if (listMessage == undefined) {
+        let noQueueMessage = new DISCORD.RichEmbed()
+            .setTitle(`Queue command for ${message.author.tag}`)
+            .addField(`Now playing`, `*${nowPlaying.songTitle}* - Asked by **${nowPlaying.requester}**`)
             .setColor("#f98257")
             .setTimestamp()
             .setAuthor(message.author.username, message.author.avatarURL)
             .setFooter(client.user.username, client.user.avatarURL)
-        message.channel.send(reponse);
+        message.channel.send(noQueueMessage);
     } else {
-        let reponse = new DISCORD.RichEmbed()
-            .setTitle(`Commande queue pour ${message.author.tag}`)
-            .addField(`En train de jouer`, `*${nowPlaying.songTitle}* - Demandé par **${nowPlaying.requester}**`)
-            .addField('Queue', resp)
+        let withQueueMessage = new DISCORD.RichEmbed()
+            .setTitle(`Queue command ${message.author.tag}`)
+            .addField(`Now playing`, `*${nowPlaying.songTitle}* - Asked by **${nowPlaying.requester}**`)
+            .addField('Queue', listMessage)
             .setColor("#f98257")
             .setTimestamp()
             .setAuthor(message.author.username, message.author.avatarURL)
             .setFooter(client.user.username, client.user.avatarURL)
-        message.channel.send(reponse);
+        message.channel.send(withQueueMessage);
     }
 
 
