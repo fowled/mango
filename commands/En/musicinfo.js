2dbd36d557952f2192b0e3213292ccba18a0b570
unknown <ma15fo43@gmail.com>
2019-07-31 23:06:39 +0200

    Translated musicinfo.js

--- commands/En/musicinfo.js
+++ commands/En/musicinfo.js
@@ -1,5 +1,7 @@
 const DISCORD = require("discord.js");
 
+// Music command
+
 exports.run = async (client, message, args, ops) => {
     let fetched = ops.active.get(message.guild.id);
 
@@ -9,18 +11,18 @@ exports.run = async (client, message, args, ops) => {
     var minutes = Math.floor(time / 60);
     var seconds = time - minutes * 60;
 
-    let reponse = new DISCORD.RichEmbed()
+    let musicInfoEmbed = new DISCORD.RichEmbed()
         .setAuthor(message.author.username, message.author.avatarURL)
-        .setTitle(`Informations pour ${message.author.tag}`)
+        .setTitle(`Information for ${message.author.tag}`)
         .setColor('#f98257')
-        .addField('Titre de la musique', `:musical_note: - **${fetched.queue[0].songTitle}**`)
-        .addField('Musique demandée par...', `L'utilisateur *${fetched.queue[0].requester}*`)
-        .addField('Auteur de la musique', fetched.queue[0].songAuthor)
-        .addField('Durée de la musique', `${minutes} minute(s) et ${seconds} secondes.`)
-        .addField("Channel d'annonces", `Le channel <#${fetched.queue[0].announceChannel}>`)
+        .addField('Music name', `:musical_note: - **${fetched.queue[0].songTitle}**`)
+        .addField('Music asked by?', `*${fetched.queue[0].requester}*`)
+        .addField('Music creator', fetched.queue[0].songAuthor)
+        .addField('Music length', `${minutes} minute(s) et ${seconds} secondes.`)
+        .addField("Announcement channel", `Le channel <#${fetched.queue[0].announceChannel}>`)
         .setURL(`https://youtube.com/${fetched.queue[0].url}`)
         .setThumbnail(`https://img.youtube.com/vi/${parseURL}/0.jpg`)
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp()
-    message.channel.send(reponse);
+    message.channel.send(musicInfoEmbed);
 }
