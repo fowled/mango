9e882b1e4cee6c48080f50f892ac3bdc1e796267
unknown <ma15fo43@gmail.com>
2019-07-31 23:21:20 +0200

    Translated ScratchMessages.js

--- commands/En/scmessages.js
+++ commands/En/scmessages.js
@@ -1,7 +1,7 @@
-// Commande liée à Scratch
-
 const DISCORD = require("discord.js");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+// Scratch command
 
 exports.run = async (client, message, args, tools) => {
     let user = args[0];
@@ -9,20 +9,20 @@ exports.run = async (client, message, args, tools) => {
     xhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
-            let object = JSON.parse(this.responseText);
-            reponse = new DISCORD.RichEmbed()
+            let parsedRequest = JSON.parse(this.responseText);
+            requestedMessages = new DISCORD.RichEmbed()
                 .setAuthor(message.author.username, message.author.avatarURL)
                 .setColor("#FF8000")
-                .setTitle("Informations messages Scratch (profil)")
-                .setDescription(`Commande Scratch : nombre de messages de l'utilisateur **${user}**...`)
-                .addField("Nombre de messages", `L'utilisateur **${user}** a actuellement **${object.count}** message(s).`)
+                .setTitle("Scratch messages information")
+                .setDescription(`How many messages does **${user}** have?`)
+                .addField("Number of messages", `**${user}** actually has **${parsedRequest.count}** message(s).`)
                 .setURL(`https://scratch.mit.edu/users/${user}`)
                 .setThumbnail(message.author.avatarURL)
                 .setTimestamp()
                 .setFooter(client.user.username, client.user.avatarURL)
-            message.channel.send(reponse);
+            message.channel.send(requestedMessages);
         } else if (this.readyState == 4 && this.responseText == "{\"code\":\"NotFound\",\"message\":\"\"}") {
-            message.reply("Utilisateur non trouvé");
+            message.reply("I did not find the user you requested.");
         }
     };
     xhttp.open("GET", `https://api.scratch.mit.edu/users/${user}/messages/count`, true);
