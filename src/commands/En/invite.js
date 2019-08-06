05cf41e89dcadfba55d2fe51855d8837773a4b7e
unknown <ma15fo43@gmail.com>
2019-08-06 16:59:47 +0200

    Converted invite.js to invite.ts UwU

--- src/commands/En/invite.js
+++
@@ -1,9 +0,0 @@
-// Guild command
-
-exports.run = async (client, message, args, tools) => {
-    message.guild.channels.get(message.channel.id).createInvite().then(invite =>
-        message.channel.send(invite.url)
-    ).catch(err => {
-        message.reply("I don't have the right perms ;( Make sure I have admin rank :wink:");
-    });
-}
---
+++ src/commands/En/invite.ts
@@ -0,0 +1,19 @@
+import * as Discord from "discord.js";
+
+// Guild command
+
+/**
+ * Génère un lien d'invitation
+ * @param {Discord.Client} Client le client
+ * @param {Discord.Message} Message le message contenant la commande
+ * @param {string[]} args les arguments de la commande
+ * @param {any} options les options
+ */
+
+export async function run(Client: Discord.Client, message: Discord.Message, args: string[], options: any) {
+    message.guild.channels.get(message.channel.id).createInvite().then((invite: Discord.Invite) =>
+        message.channel.send(invite.url)
+    ).catch(err => {
+        message.reply("I don't have the right perms ;( Make sure I have the admin rank :wink:");
+    });
+}
