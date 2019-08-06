3bb0eacdc495a7f76eed516c9b8392540a54571d
unknown <ma15fo43@gmail.com>
2019-08-06 13:21:57 +0200

    Helped by Smrman to convert this file, thanks!

--- src/commands/En/userinfo.ts
+++ src/commands/En/userinfo.ts
@@ -2,85 +2,40 @@ import * as Discord from "discord.js";
 
 // Member command
 
-export async function run(Client: Discord.Client, message: Discord.Message , args: string[], options: any) {
+/**
+ * Affiche les infos sur un utilisteur
+ * @param {Discord.Client} Client le client
+ * @param {Discord.Message} Message le message contenant la commande
+ * @param {string[]} args les arguments de la commande
+ * @param {any} options les options
+ */
 
-    const userInfo = message.mentions.users.first();
-    if (userInfo) {
-        const memberInfo = message.guild.member(userInfo);
-        var userGameStatus = userInfo.presence.game;
+export async function run(Client: Discord.Client, message: Discord.Message , args: string[], options: any): Promise<void> {
 
-        if (memberInfo) {
-            var userRoles: string[];
+    const selectedUser: Discord.User = message.mentions.users.size > 0 ? (message.guild.member(message.mentions.users.first()) ? message.mentions.users.first() : null) : message.author;
 
-            memberInfo.roles.forEach(role => userRoles.push(role.name));
-
-            userRoles.shift();
-
-            if (userRoles.length == 0) {
-                var userRolesRichEmbed = "This user has no role.";
-            } else {
-                var userRolesRichEmbed = userRoles.join(", ")
-            }
-
-            if (!userGameStatus) {
-                var userGameStatusRichEmbed = "This user has no game";
-            }
-
-            var userinfoRichEmbed = new Discord.RichEmbed()
-                .setTitle(`User information for ${userInfo.username}`)
-                .setAuthor(userInfo.username, userInfo.avatarURL)
-                .setDescription("User information")
-                .setThumbnail(userInfo.avatarURL)
-                .setTimestamp()
-                .addField("Username", userInfo.username, true)
-                .addField("Tag", userInfo.discriminator, true)
-                .addField("ID", userInfo.id, true)
-                .addField("Status", userInfo.presence.status, true)
-                .addField("Game status", userGameStatusRichEmbed, true)
-                .addField("Arrival date", memberInfo.joinedAt.toLocaleString())
-                .addField("Roles owned in the server", userRolesRichEmbed)
-                .addField("The date the account was created", userInfo.createdAt.toLocaleString())
-                .setColor(Math.floor(Math.random() * 16777214) + 1)
-                .setFooter(Client.user.username, Client.user.avatarURL)
-            message.channel.send(userinfoRichEmbed);
-        } else {
-            message.channel.send("Tagged user is not in the server :frowning:");
-        }
-
-    } else {
-        var userRoles: string[];
-        var userPresence = message.author.presence.game;
-
-        message.member.roles.forEach(role => userRoles.push(role.name));
-
-        userRoles.shift();
-
-        if (userRoles.length == 0) {
-            userRolesRichEmbed = "You don't have any role in the server";
-        } else {
-            userRolesRichEmbed = userRoles.join(", ");
-        }
-
-        if (!userPresence) {
-            var userPresenceRichEmbed = "You don't have any active game on your PC.";
-        }
-
-        var userinfoRichEmbedYourself = new Discord.RichEmbed()
-            .setTitle(`User information for ${message.author.username}`)
-            .setAuthor(message.author.username, message.author.avatarURL)
+    if (selectedUser) { // dans le même serveur
+        var userinfoRichEmbed: Discord.RichEmbed = new Discord.RichEmbed()
+            .setTitle(`User information for ${selectedUser.username}`)
+            .setAuthor(selectedUser.username, selectedUser.avatarURL)
             .setDescription("User information")
-            .setThumbnail(message.author.avatarURL)
+            .setThumbnail(selectedUser.avatarURL)
             .setTimestamp()
-            .addField("Username", message.author.username, true)
-            .addField("Tag", message.author.discriminator, true)
-            .addField("ID", message.author.id)
-            .addField("Status", message.member.presence.status, true)
-            .addField("Game status", userPresenceRichEmbed, true)
-            .addField("Arrival date", message.member.joinedAt.toLocaleString())
-            .addField("Roles owned in the server", userRolesRichEmbed)
-            .addField("The date the account was created on", message.author.createdAt.toLocaleString())
+            .addField("Username", selectedUser.username, true)
+            .addField("Tag", selectedUser.discriminator, true)
+            .addField("ID", selectedUser.id, true)
+            .addField("Status", selectedUser.presence.status, true)
+            .addField("Game status", selectedUser.presence.game ? selectedUser.presence.game.name : "Not currently playing any game", true);
+        if (message.guild.member(message.mentions.users.first())) {
+            userinfoRichEmbed.addField("Arrival date", message.guild.member(selectedUser).joinedAt.toLocaleString())
+            .addField("Roles owned in the server", message.guild.member(selectedUser).roles.array().splice(1).map(role => role.name).length === 0 ? "This user has no role" : message.guild.member(selectedUser).roles.array().splice(1).map(role => role.name).join(", "));
+        }
+        userinfoRichEmbed.addField("The date the account was created", selectedUser.createdAt.toLocaleString())
             .setColor(Math.floor(Math.random() * 16777214) + 1)
-            .setFooter(Client.user.username, Client.user.avatarURL)
-        message.channel.send(userinfoRichEmbedYourself);
+            .setFooter(Client.user.username, Client.user.avatarURL);
+            
+        message.channel.send(userinfoRichEmbed);
+    } else {
+        message.channel.send("Tagged user is not in the server :frowning:");
     }
 }
