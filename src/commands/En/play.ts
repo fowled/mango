8b442e9478711909bbf5effa690ae3ee91bb4543
Max <ma15fo43@gmail.com>
2020-02-01 13:27:52 +0100

    Fixed play command!

--- src/commands/En/play.ts
+++ src/commands/En/play.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import ytdl from "ytdl-core";
+import * as Logger from "../../utils/Logger";
 
 // Music command
 
@@ -12,45 +13,41 @@ import ytdl from "ytdl-core";
  */
 export async function run(client: Discord.Client, message: Discord.Message, args: string[], ops: any): Promise<void> {
 	const voiceChannel: Discord.VoiceChannel = message.member.voiceChannel;
-	const permissions: Discord.PermissionResolvable = voiceChannel.permissionsFor(message.client.user);
 
 	if (!voiceChannel) {
 		message.channel.send("I'm sorry, but you need to be connected in a voice channel to start playing :musical_note:");
 		return;
 	}
+
+	const permissions: Discord.PermissionResolvable = voiceChannel.permissionsFor(message.client.user);
+
 	if (!args[0]) {
 		message.reply("You must provide a link to the song I should play!");
 		return;
 	}
+
 	if (!permissions.has("CONNECT")) {
 		message.reply("I can't connect to the channel because I don't have permission to.");
 		return;
 	}
+
 	if (!permissions.has("SPEAK")) {
 		message.reply("Welp, it looks like I can't speak. Like, I can't play music. Add me the permission to!");
 		return;
 	}
 
-	voiceChannel.join()
-	.then((connection: Discord.VoiceConnection): void => {
-		connection
-			.playFile("encore.mp3");
-			//.playStream(ytdl(args[0], {filter: "audioonly"}));
-			//.setVolumeLogarithmic(5 / 5);
-	})
-	.catch((error: Error): void => {
-		console.error(`I couldn't join the voice channel, because of the following error: ${error}`);
-		message.channel.send(`Unexpected error, please try again the command. \`\`\`${error}\`\`\``);
-		return;
-	});
+	const validate: boolean = await ytdl.validateURL(args[0]);
 
+	if (!validate) {
+		message.reply("Hmm, the URL isn't valid... please try again.");
+	}
 
-	/*.on('end', () => {
-		console.log("Song ended."); // FIXME: mis en commentaire, sinon ça quitte le channel en disant que la chanson est terminée alors que ça n'a même pas commencé à la jouer...
-		voiceChannel.leave();
-	})
-	.on('error', error => {
-		console.error(error);
-	});*/
+	const info: ytdl.videoInfo = await ytdl.getInfo(args[0]);
+	const connection: Discord.VoiceConnection = await message.member.voiceChannel.join();
+	const dispatcher: Discord.StreamDispatcher = await connection.playStream(ytdl(args[0], { filter: "audioonly" }));
 
+	message.channel.send(`Now playing: **${info.title}**`);
+	Logger.log(`Playing ${info.title} on guild: ${message.member.guild.name}`)
+
+	dispatcher.setVolumeLogarithmic(5 / 5);
 }
