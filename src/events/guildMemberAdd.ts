6346b0de0d37e0ffab64e645c0f64c2067a7d3c7
Ma15fo43 <ma15fo43@gmail.com>
2020-04-06 23:10:03 +0200

    now displays a picture when joining/leaving guild.

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import * as fs from "fs";
+import * as canvaslib from "canvas";
 
 export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 	let savedWelcChan = fs.readFileSync("database/welcome/channels.json", "utf-8");
@@ -10,15 +11,6 @@ export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 		return;
 	}
 
-	const welcomeRichEmbed = new Discord.RichEmbed()
-		.setTitle("A member just joined the guild :inbox_tray:")
-		.setDescription(`Welcome ${member}! We wish you to have fun in **${member.guild.name}**. Help message: type *!infohelp* in server!`)
-		.setAuthor(member.user.username, member.user.avatarURL)
-		.setFooter(Client.user.username, Client.user.avatarURL)
-		.setColor("0FB1FB")
-	// @ts-ignore
-	welcomeChannel.send(welcomeRichEmbed);
-
 	const welcUserRichEmbed = new Discord.RichEmbed()
 		.setTitle("Welcome!")
 		.setDescription(`Welcome ${member}! We wish you to have fun in **${member.guild.name}**. Help message: type *!infohelp* in server!`)
@@ -26,4 +18,33 @@ export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 		.setFooter(Client.user.username, Client.user.avatarURL)
 		.setColor("0FB1FB")
 	Client.users.get(member.id).send(welcUserRichEmbed);
+
+    const canvas = canvaslib.createCanvas(700, 250);
+    const ctx = canvas.getContext("2d");
+
+    const background = await canvaslib.loadImage("./background.png");
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.font = "35px Caviar Dreams"; // displays on the picture the member tag
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = "center";
+    ctx.fillText(`Welcome \n${member.user.tag}`, canvas.width - 400, canvas.height / 2.3);
+
+    ctx.font = "25px Caviar Dreams"; // displays on the picture the member tag
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = "center";
+    ctx.fillText(`Member #${member.guild.members.size}`, canvas.width - 200, canvas.height / 1.30);
+
+    ctx.beginPath(); // rounded profile pic
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    const avatar = await canvaslib.loadImage(member.user.displayAvatarURL);
+    ctx.drawImage(avatar, 25, 25, 100, 100);
+
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	// @ts-ignore
+	welcomeChannel.send(attachment);
 };
--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import * as fs from "fs";
+import * as canvaslib from "canvas";
 
 export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 	let savedWelcChan = fs.readFileSync("database/welcome/channels.json", "utf-8");
@@ -18,5 +19,37 @@ export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 		.setColor("4F6A77")
 	// @ts-ignore
 	channel.send(welcomeRichEmbed);
+
+	const canvas = canvaslib.createCanvas(700, 250);
+    const ctx = canvas.getContext("2d");
+
+    const background = await canvaslib.loadImage("./leave_background.png");
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.font = "35px Caviar Dreams"; // displays on the picture the member tag
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = "center";
+    ctx.fillText(`Goodbye \n${member.user.tag}`, canvas.width - 400, canvas.height / 2.3);
+
+    ctx.font = "25px Caviar Dreams"; // displays on the picture the member tag
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = "center";
+    ctx.fillText(`Members: ${member.guild.members.size}`, canvas.width - 200, canvas.height / 1.30);
+
+    ctx.beginPath(); // rounded profile pic
+    ctx.arc(85, 85, 60, 0, Math.PI * 2, true);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 10;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.clip();
+
+    const avatar = await canvaslib.loadImage(member.user.displayAvatarURL);
+    ctx.drawImage(avatar, 25, 25, 120, 120);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+	
+	// @ts-ignore
+	channel.send(attachment);
 };
 
