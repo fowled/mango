18ad940fd5cf20269f28379db4a6b1f41cce328e
Ma15fo43 <ma15fo43@gmail.com>
2020-04-08 21:37:43 +0200

    made changes related to assets folder

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -23,7 +23,7 @@ export default async (Client: Discord.Client, member: Discord.GuildMember) => {
 	const canvas = canvaslib.createCanvas(700, 250);
     const ctx = canvas.getContext("2d");
 
-    const background = await canvaslib.loadImage("./leave_background.png");
+    const background = await canvaslib.loadImage("./assets/leave_background.png");
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
     ctx.font = "35px Caviar Dreams"; // displays on the picture the member tag
