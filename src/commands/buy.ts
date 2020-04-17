3873f9af0f60d1ce869fc994b215dbafa0423288
Ma15fo43 <ma15fo43@gmail.com>
2020-04-17 22:44:51 +0200

    bug fixes

--- src/commands/buy.ts
+++ src/commands/buy.ts
@@ -5,7 +5,7 @@ import * as Logger from "../utils/Logger";
 // Fun command
 
 /**
- * Sells something to the black market (lmfao)
+ * Buys something on the black market (lmfao)
  * @param {Discord.Client} Client the client
  * @param {Discord.Message} Message the message that contains the command name
  * @param {string[]} args the command args
@@ -13,22 +13,34 @@ import * as Logger from "../utils/Logger";
  */
 export async function run(Client: Discord.Client, message: Discord.Message, args: string[], ops: any) {
     const ID = args[0];
-    
+
     if (!ID) {
         return message.reply("In order to buy something, you must provide the item's ID.");
     }
-    
+
     let content = JSON.parse(fs.readFileSync('database/market/items.json', 'utf8'));
     let money = JSON.parse(fs.readFileSync('database/money/data.json', 'utf8'));
-    money = money[message.author.id] == undefined ? "0" : money[message.author.id];
 
-    console.log(money);
-
-    if (money < content[ID]["price"]) {
-        return message.reply(`You must have \`${content[ID]["price"] - money}\` more dollars to get this item. :frowning:`);
-    } else {
-        delete(content[ID]);
+    if (money[message.author.id] == undefined) {
+        money[message.author.id] = 500;
+        fs.writeFileSync("database/money/data.json", JSON.stringify(money));
     }
+
+    let refreshedMoney = JSON.parse(fs.readFileSync('database/money/data.json', 'utf8'));
+    let seller = content[ID]["sellerID"];
+
+    if (refreshedMoney[message.author.id] < content[ID]["price"]) {
+        return message.reply(`You must have \`${content[ID]["price"] - refreshedMoney[message.author.id]}\` more dollars to get this item. :frowning:`);
+    } else if (message.author.id == seller) {
+        return message.reply("You can't buy your own item...");
+    } else {
+        refreshedMoney[message.author.id] -= content[ID]["price"];
+        refreshedMoney[seller] += content[ID]["price"];
+        message.reply(`Item **${content[ID]["name"]}** successfully bought for *${content[ID]["price"]}$*.`);
+        delete (content[ID]);
+    }
+
+    fs.writeFileSync("database/money/data.json", JSON.stringify(refreshedMoney));
 
     fs.writeFile(`database/market/items.json`, JSON.stringify(content), function (err) {
         if (err) {
