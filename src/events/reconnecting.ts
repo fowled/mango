ac88580615005e624ee0302cfba4d305ecc1f0af
Ma15fo43 <ma15fo43@gmail.com>
2020-04-26 18:44:07 +0200

    deleted a useless event

--- src/events/reconnecting.ts
+++
@@ -1,7 +0,0 @@
-import * as Discord from "discord.js";
-
-import * as Logger from "./../utils/Logger";
-
-export default async (Client: Discord.Client) => {
-    Logger.log(`Attempting to reconnect the bot...`);
-};
