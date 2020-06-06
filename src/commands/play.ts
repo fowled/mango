7401df76626a1464428b2549bc71ac8e975a3f4b
Ma15fo43 <ma15fo43@gmail.com>
2020-06-06 13:25:09 +0200

    play command enhancement

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -12,6 +12,8 @@ import * as Logger from ".././utils/Logger";
  * @param {any} options some options
  */
 export async function run(client: Discord.Client, message: Discord.Message, args: string[], ops: any): Promise<void> {
+	const queue = ops.queue;
+	const serverQueue = queue.get(message.guild.id);
 	const voiceChannel: Discord.VoiceChannel = message.member.voice.channel;
 
 	if (!voiceChannel) {
@@ -32,7 +34,7 @@ export async function run(client: Discord.Client, message: Discord.Message, args
 	}
 
 	if (!permissions.has("SPEAK")) {
-		message.reply("Welp, it looks like I can't speak. Like, I can't play music. Add me the permission to!");
+		message.reply("Welp, it looks like I can't speak. Means that I can't play music. Add me the permission to!");
 		return;
 	}
 
@@ -42,12 +44,68 @@ export async function run(client: Discord.Client, message: Discord.Message, args
 		message.reply("Hmm, the URL isn't valid... please try again.");
 	}
 
-	const info: ytdl.videoInfo = await ytdl.getInfo(args[0]);
-	const connection: Discord.VoiceConnection = await message.member.voice.channel.join();
-	const dispatcher: Discord.StreamDispatcher = await connection.play(ytdl(args[0], { filter: "audioonly" }));
+	const songInfo: ytdl.videoInfo = await ytdl.getInfo(args[0]);
 
-	message.channel.send(`Now playing: **${info.title}**`);
-	Logger.log(`Playing ${info.title} on guild: ${message.member.guild.name}`)
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+		requester: message.author.tag,
+		author: songInfo.author,
+		length: songInfo.length_seconds,
+		id: songInfo.video_id,
+	}
 
-	dispatcher.setVolumeLogarithmic(5 / 5);
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+
+		queue.set(message.guild.id, queueConstruct);
+		queueConstruct.songs.push(song);
+
+		message.channel.send(`Currently getting info from **${songInfo.title}**...`);
+
+		try {
+			var connection: Discord.VoiceConnection = await message.member.voice.channel.join();
+			queueConstruct.connection = connection;
+			play(message.guild, queueConstruct.songs[0]);
+		} catch (err) {
+			Logger.error(err);
+			queue.delete(message.guild.id);
+			message.channel.send("Couldn't join the voice channel.");
+		}
+	} else {
+		serverQueue.songs.push(song);
+		message.channel.send(`**${song.title}** has been added to the queue.`);
+	}
+
+	function play(guild: Discord.Guild, song) {
+		const serverQueue = queue.get(guild.id);
+
+		if (!song) {
+			serverQueue.voiceChannel.leave();
+			queue.delete(guild.id);
+			return;
+		}
+
+		const dispatcher: Discord.StreamDispatcher = serverQueue.connection.play(ytdl(song.url), { filter: "audioonly" })
+			.on("finish", () => {
+				Logger.log(`Finished playing song "${args[0]}"`);
+				serverQueue.songs.shift();
+				play(guild, serverQueue.songs[0]);
+			})
+			.on("error", error => {
+				Logger.error(error);
+			});
+
+		dispatcher.setVolumeLogarithmic(5 / 5);
+
+		message.channel.send(`Now playing: **${songInfo.title}**`);
+		Logger.log(`Playing ${songInfo.title} on guild: ${message.member.guild.name}`);
+	}
 }
