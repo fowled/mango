6fa40fd75c9608bf7a098d88b45242a4a62b48a3
Ma15fo43 <ma15fo43@gmail.com>
2020-06-20 18:36:36 +0200

    wtf am I even doing to these

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,14 +4,14 @@ import * as Logger from "./../utils/Logger";
 
 export default async (Client: Discord.Client) => {
 	Logger.log("Client is ready");
-	
+
 	setInterval(() => {
 		const activitiesList = [`${Client.users.cache.size} users | ${Client.guilds.cache.size} guilds`, "ma!help", "@Mazz#0270", "the verified badge"]
 		Client.user.setPresence({
 			activity: {
 				name: activitiesList[Math.floor(Math.random() * activitiesList.length)],
 				type: "WATCHING",
-				
+
 			},
 			status: "online",
 		});
