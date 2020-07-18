8f8fc19083c86cc469d473a811bc0e9d2dc9ef6b
ma15fo43 <ma15fo43@gmail.com>
2020-07-18 20:08:33 +0200

    no more custom prefix

--- src/events/message.ts
+++ src/events/message.ts
@@ -11,43 +11,33 @@ export default async (Client: Discord.Client, message: Discord.Message) => {
 		return;
 	}
 
-	let prefix = JSON.parse(Fs.readFileSync(`./database/prefixes/prefixes.json`, "utf8"));
+	const prefix = "ma!";
 
 	if (message.mentions.has(Client.user, { ignoreDirect: false, ignoreEveryone: true, ignoreRoles: true }) && message.content.split(" ").length == 1) {
-		if (prefix[message.author.id] == undefined) {
-			prefix = "ma!";
-		} else {
-			prefix = prefix[message.author.id];
-		}
 		message.reply(`Hey, I'm Mango! Your current prefix is \`${prefix}\` \n→ help message: \`${prefix}help\` <a:check:690888185084903475>`);
 	}
 
 	Xp.checkXP(message);
 
-	Fs.readFile(`./database/prefixes/prefixes.json`, "utf8", (err: Error, data): void => {
-		data = JSON.parse(data);
-		const prefix: string = data[message.author.id] == undefined ? "ma!" : data[message.author.id];
+	const msg: string = message.content;
+	const args: string[] = message.content.slice(prefix.length).trim().split(" ");
+	const cmd: string = args.shift().toLowerCase();
 
-		const msg: string = message.content;
-		const args: string[] = message.content.slice(prefix.length).trim().split(" ");
-		const cmd: string = args.shift().toLowerCase();
+	if (!msg.startsWith(prefix)) {
+		return checkCustomCommands();
+	}
 
-		if (!msg.startsWith(prefix)) {
-			return checkCustomCommands();
-		}
+	let ops: {} = {
+		queue: queue
+	}
 
-		let ops: {} = {
-			queue: queue
-		}
+	try {
+		require(checkFolders(cmd)).run(Client, message, args, ops);
+		Logger.log(`${message.author.tag} just used the ${cmd} power in ${message.guild.name}.`);
+	} catch (err) {
+		Logger.log(`The command ${message.author.tag} tried to call in ${message.guild.name} doesen't seem to exist.`);
+	}
 
-		try {
-			require(checkFolders(cmd)).run(Client, message, args, ops);
-			Logger.log(`${message.author.tag} just used the ${cmd} power in ${message.guild.name}.`);
-		} catch (err) {
-			Logger.log(`The command ${message.author.tag} tried to call in ${message.guild.name} doesen't seem to exist.`);
-		}
-
-	});
 
 	function checkCustomCommands() {
 		let content = JSON.parse(Fs.readFileSync('./database/commands/commands.json', 'utf8'));
@@ -70,7 +60,7 @@ export default async (Client: Discord.Client, message: Discord.Message) => {
 
 		folders.forEach(folder => {
 			files = Fs.readdirSync(`./src/commands/${folder}`);
-			
+
 			files.forEach(file => {
 				if (file.split(".")[0] == command) {
 					return finalPath = `./../commands/${folder}/${file.split(".")[0]}.js`;
