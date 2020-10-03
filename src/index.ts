ec65b5c467597f33f14ccf6a1ac31d966079ce95
ma15fo43 <ma15fo43@gmail.com>
2020-10-03 21:52:47 +0200

    Big changes - Sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 ﻿import * as Discord from "discord.js";
+import * as Sequelize from "sequelize";
 
 import * as Binder from "./utils/EventsBinder";
 
@@ -8,6 +9,12 @@ const Client: Discord.Client = new Discord.Client({ fetchAllMembers: true });
 
 export const talkedRecently = new Set();
 export const queue = new Map();
+export const sequelizeinit = new Sequelize.Sequelize("database", "username", "password", {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: "database/db.sqlite",
+});
 
 Client.on('error', console.error);
 
