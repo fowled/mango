d36290828dc44fe286ed48aef37c46c72ab98dfb
ma15fo43 <ma15fo43@gmail.com>
2020-10-03 21:54:42 +0200

    added models folder

---
+++ src/models/models.ts
@@ -0,0 +1,54 @@
+import * as Sequelize from "sequelize";
+import { sequelizeinit } from "../index";
+
+export async function defModels() {
+    sequelizeinit.define("marketItems", {
+        name: {
+            type: Sequelize.STRING,
+            unique: true
+        },
+        price: Sequelize.TEXT,
+        seller: Sequelize.STRING,
+        sellerID: Sequelize.STRING,
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        }
+    });
+
+    sequelizeinit.define("inventoryItems", {
+        name: {
+            type: Sequelize.STRING,
+            unique: true
+        },
+        price: Sequelize.TEXT,
+        seller: Sequelize.STRING,
+        authorID: Sequelize.STRING,
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        }
+    });
+
+    sequelizeinit.define("moneyAcc", {
+        idOfUser: Sequelize.STRING,
+        money: Sequelize.INTEGER
+    });
+
+    sequelizeinit.define("welChannels", {
+        idOfGuild: Sequelize.STRING,
+        idOfChannel: Sequelize.STRING
+    });
+
+    sequelizeinit.define("logChannels", {
+        idOfGuild: Sequelize.STRING,
+        idOfChannel: Sequelize.STRING
+    });
+
+    sequelizeinit.define("ranks", {
+        idOfUser: Sequelize.STRING,
+        xp: Sequelize.INTEGER
+    });
+}
