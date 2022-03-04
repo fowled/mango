3bb960aa6fb29f7a8728afe2bb6adda41030c63c
Max <ma15fo43@gmail.com>
2022-03-04 13:44:53 +0100

    ✨ API update

--- api/server.ts
+++ api/server.ts
@@ -12,10 +12,10 @@ import { client } from "../src/index";
 const app: Express = express();
 
 declare module "express-session" {
-    interface SessionData {
-        token: string;
-        refresh_token: string;
-    }
+	interface SessionData {
+		token: string;
+		refresh_token: string;
+	}
 }
 
 app.use(session({ secret: process.env.SESSION_SECRET, cookie: { secure: false }, resave: false, saveUninitialized: true }));
@@ -26,124 +26,125 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors({
-    origin: [
-        'http://localhost:3000',
-    ],
-    credentials: true,
-    exposedHeaders: ['set-cookie']
-}));
+app.use(
+	cors({
+		origin: ["http://localhost:3000", "https://mango.bot"],
+		credentials: true,
+		exposedHeaders: ["set-cookie"],
+	})
+);
 
-app.get("/api", async function (req, res) {      
-    return res.send({ message: "Welcome to Mango's API!" });
+app.get("/", async function (req, res) {
+	return res.send({ message: "Welcome to Mango's API!" });
 });
 
-app.get("/api/token", async function (req, res) {
-    const code: string = req.query.code as string;
+app.get("/token", async function (req, res) {
+	const code: string = req.query.code as string;
 
-    const fetchToken = await fetch("https://discord.com/api/oauth2/token", {
-        method: "POST",
+	const fetchToken = await fetch("https://discord.com/api/oauth2/token", {
+		method: "POST",
 
-        body: new URLSearchParams({
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            code,
-            grant_type: "authorization_code",
-            redirect_uri: process.env.REDIRECT_URI,
-            scope: "identify guilds",
-        }),
+		body: new URLSearchParams({
+			client_id: process.env.CLIENT_ID,
+			client_secret: process.env.CLIENT_SECRET,
+			code,
+			grant_type: "authorization_code",
+			redirect_uri: process.env.REDIRECT_URI,
+			scope: "identify guilds",
+		}),
 
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-    });
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	});
 
-    const parseResponse = await fetchToken.json();
+	const parseResponse = await fetchToken.json();
 
-    req.session.token = parseResponse.access_token;
-    req.session.refresh_token = parseResponse.refresh_token;
+	req.session.token = parseResponse.access_token;
+	req.session.refresh_token = parseResponse.refresh_token;
 
-    return res.send({ message: "Unauthorized" });
+	return res.send({ message: "Unauthorized" });
 });
 
-app.get("/api/authed", async function (req, res) {
-    if (req.session.token) {
-        return res.send(true);
-    } else {
-        return res.send(false);
-    }
+app.get("/authed", async function (req, res) {
+	if (req.session.token) {
+		return res.send(true);
+	} else {
+		return res.send(false);
+	}
 });
 
-app.get("/api/stats", async function (req, res) {
-    const retrieveUsers: number = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+app.get("/stats", async function (req, res) {
+	const retrieveUsers: number = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
 
-    const dataObject = {
-        users: retrieveUsers,
-        servers: client.guilds.cache.size
-    }
+	const dataObject = {
+		users: retrieveUsers,
+		servers: client.guilds.cache.size,
+	};
 
-    return res.status(200).json({
-        message: dataObject
-    });
+	return res.status(200).json({
+		message: dataObject,
+	});
 });
 
-app.get("/api/me", async function (req, res) {
-    const userResult = await fetch("https://discord.com/api/users/@me", {
-        headers: {
-            authorization: `Bearer ${req.session.token}`,
-        },
-    }).then((res => res.json()));
+app.get("/me", async function (req, res) {
+	const userResult = await fetch("https://discord.com/api/users/@me", {
+		headers: {
+			authorization: `Bearer ${req.session.token}`,
+		},
+	}).then((res) => res.json());
 
-    return res.send(userResult);
+	return res.send(userResult);
 });
 
-app.get("/api/managed", async function (req, res) {
-    const managedServersArray: {}[] = [];
-    const botNotInGuild: {}[] = [];
+app.get("/managed", async function (req, res) {
+	const managedServersArray = [];
 
-    const userGuilds = await fetch("https://discord.com/api/users/@me/guilds", {
-        headers: {
-            authorization: `Bearer ${req.session.token}`
-        }
-    });
+	const userGuilds = await fetch("https://discord.com/api/users/@me/guilds", {
+		headers: {
+			authorization: `Bearer ${req.session.token}`,
+		},
+	});
 
-    if (userGuilds.status !== 200) return res.status(userGuilds.status).send(userGuilds.statusText);
+	if (userGuilds.status !== 200) return res.status(userGuilds.status).send(userGuilds.statusText);
 
-    for (let guild of await userGuilds.json()) {
-        const permissionsBitfield: boolean = new BitField(guild.permissions).has("32");
-        const isBotInGuild: boolean = client.guilds.resolve(guild.id) != null;
+	for (let guild of await userGuilds.json()) {
+		const permissionsBitfield: boolean = new BitField(guild.permissions).has("32");
+		const isBotInGuild: boolean = client.guilds.resolve(guild.id) != null;
 
-        if (permissionsBitfield && isBotInGuild) {
-            managedServersArray.push(guild);
-        } else if (permissionsBitfield) {
-            botNotInGuild.push(guild);
-        }
-    }
+		guild.bot = isBotInGuild;
 
-    return res.status(200).send({ managedServersArray, botNotInGuild });
+		if (permissionsBitfield) {
+			managedServersArray.push(guild);
+		}
+	}
+
+	managedServersArray.sort((a) => (a.bot === false ? 1 : -1));
+
+	return res.status(200).send({ managedServersArray });
 });
 
-app.get("/api/manage/:guildId", async function (req, res) {
-    const guildId: string = req.params.guildId;
-    const guildInfo: Guild = client.guilds.resolve(guildId);
+app.get("/manage/:guildId", async function (req, res) {
+	const guildId: string = req.params.guildId;
+	const guildInfo: Guild = client.guilds.resolve(guildId);
 
-    if (guildInfo == null) {
-        return res.status(403).send({ message: "403: Unauthorized - bot isn't in guild." });
-    }
+	if (guildInfo == null) {
+		return res.status(403).send({ message: "403: Unauthorized - bot isn't in guild." });
+	}
 
-    return res.status(200).send({ guild: guildInfo });
+	return res.status(200).send({ guild: guildInfo });
 });
 
 app.use((req, res, next) => {
-    const error: Error = new Error("404: not found");
+	const error: Error = new Error("404: not found");
 
-    return res.status(404).json({
-        message: error.message
-    });
+	return res.status(404).json({
+		message: error.message,
+	});
 });
 
 const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => {
-    log(`Server is up and running at http://localhost:${port}`);
+	log(`Server is up and running at http://localhost:${port}`);
 });
