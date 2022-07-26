8c3508c8251211aa15bf0eab03e5bc472fbf7416
Max <ma15fo43@gmail.com>
2022-07-26 11:44:09 +0200

    Change Vite default port

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [svelte(), tsconfigPaths({ loose: true, root: "./" })],
 	server: {
-		port: 3000,
+		port: 3002,
 	},
 });
