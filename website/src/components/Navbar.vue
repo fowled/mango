9068b5c632c7e8bec8c331ba8fbd9b8e69ff1608
fowled <ma15fo43@gmail.com>
2024-04-19 22:59:34 +0200

    Fix the navbar

--- website/src/components/Navbar.vue
+++ website/src/components/Navbar.vue
@@ -49,7 +49,7 @@ onMounted(async () => {
 					<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-semibold">
 						<li v-for="element in navigation">
 							<a
-								:href="navelement.link"
+								:href="element.link"
 								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>
 								{{ element.name }}
